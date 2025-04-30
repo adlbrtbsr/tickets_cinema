649e448d4ce488d2fae251897c38e01292b357a8
Wojciech <adlbrtbsr@wojek-debian.wap>
2025-04-30 22:15:38 +0200

    initial django and dockerfiles config

--- tickets/settings/base.py
+++ tickets/settings/base.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+#TODO: look through settings to make sure everything is correct
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
