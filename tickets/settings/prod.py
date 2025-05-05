3805dd6bd51d501849556c033d1a120dd62e017c
Wojciech <adlbrtbsr@wojek-debian.wap>
2025-05-05 15:26:00 +0200

    feat: set up workflows to use black and isort linters

--- tickets/settings/prod.py
+++ tickets/settings/prod.py
@@ -1,12 +1,13 @@
 import os
+
 from .base import *
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = False
 
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    #TODO set up prod database
+    # TODO set up prod database
 }
