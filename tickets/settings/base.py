b8049a01c458e5f6645b58f4dae87824f0aa371b
Wojciech <adlbrtbsr@wojek-debian.wap>
2025-05-05 18:07:56 +0200

    feat: split INSTALLED_APPS setting into django, third_party and local apps

--- tickets/settings/base.py
+++ tickets/settings/base.py
@@ -16,16 +16,19 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
 ]
+THIRD_PARTY_APPS = []
+LOCAL_APPS = [
+    'core'
+]
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
