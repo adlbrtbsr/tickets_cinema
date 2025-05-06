2b47b9d6c073b220eb5f0e825ac4a47c77c323a9
Wojciech <adlbrtbsr@wojek-debian.wap>
2025-05-06 18:18:06 +0200

    fix: removed redundancies in field names, adjusted max length of charfields

--- core/models.py
+++ core/models.py
@@ -7,40 +7,40 @@ class User(AbstractUser):
 
 
 class Genre(models.Model):
-    genre_name = models.CharField(max_length=100)
-    genre_is_for_adults = models.BooleanField(default=False)
+    name = models.CharField(max_length=26)
+    is_for_adults = models.BooleanField(default=False)
 
 
 class Actor(models.Model):
-    actor_name = models.CharField(max_length=100)
-    actor_age = models.PositiveIntegerField()
-    actor_nationality = models.CharField(max_length=100)
+    name = models.CharField(max_length=60)
+    age = models.PositiveIntegerField()
+    nationality = models.CharField(max_length=54)
 
 
 class Movie(models.Model):
-    movie_title = models.CharField(max_length=100)
-    movie_genres = models.ManyToManyField(Genre)
-    movie_actors = models.ManyToManyField(Actor)
-    movie_duration = models.PositiveIntegerField()
+    title = models.CharField(max_length=185)
+    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor)
+    duration = models.PositiveIntegerField()
 
 
 class CinemaHall(models.Model):
-    cinema_hall_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=20)
 
 
 class Seat(models.Model):
-    seat_row = models.PositiveIntegerField()
-    seat_number = models.PositiveIntegerField()
-    seat_hall = models.ForeignKey(CinemaHall, on_delete=models.PROTECT)
+    row = models.PositiveIntegerField()
+    number = models.PositiveIntegerField()
+    hall = models.ForeignKey(CinemaHall, on_delete=models.PROTECT)
 
 
 class MovieScreening(models.Model):
-    movie_screening_movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    movie_screening_date = models.DateTimeField()
-    movie_screening_hall = models.ForeignKey(CinemaHall, on_delete=models.PROTECT)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    hall = models.ForeignKey(CinemaHall, on_delete=models.PROTECT)
 
 
 class Ticket(models.Model):
-    ticket_movie_screening = models.ForeignKey(MovieScreening, on_delete=models.CASCADE)
-    ticket_seat = models.ForeignKey(Seat, on_delete=models.PROTECT)
-    ticket_price = models.PositiveIntegerField()
+    movie_screening = models.ForeignKey(MovieScreening, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.PROTECT)
+    price = models.PositiveIntegerField()
